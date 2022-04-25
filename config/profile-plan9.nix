e6fe893474b3b9fbe6026fa446fdc895c3c6872a
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-25 10:27:41 +0100

    Move around some files

---
+++ config/profile-plan9.nix
@@ -0,0 +1,8 @@
+{
+	home.file."lib/profile".text = ''
+		path=(/Users/lillycham/go/bin /Users/lillycham/.nix-profile/bin /nix/var/nix/profiles/default/bin /Users/lillycham/.cabal/bin /Library/Frameworks/Python.framework/Versions/3.9/bin /Users/lillycham/Library/Python/3.9/bin /usr/local/plan9/bin /Library/TeX/texbin /Users/lillycham/.ghcup/bin /usr/local/bin /opt/local/bin /usr/bin /bin /usr/sbin /sbin .)
+		prompttext=`{ whoami }
+		prompt=(`{ whoami }' @ mirai % ' '    ')
+		NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+	'';
+}
