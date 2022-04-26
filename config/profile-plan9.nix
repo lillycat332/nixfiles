26f1c0bbdb4eae5043f1edb3d2880b899fe67af8
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-26 09:12:11 +0100

    remoev stuff

--- config/profile-plan9.nix
+++
@@ -1,5 +0,0 @@
-{
-  # This file is the ~/lib/profile file, which is similar to .bashrc for the Plan 9 'rc' shell.
-  home.file."lib/profile".text =
-    "	path=(/Users/lillycham/go/bin /Users/lillycham/.nix-profile/bin /nix/var/nix/profiles/default/bin /Users/lillycham/.cabal/bin /Library/Frameworks/Python.framework/Versions/3.9/bin /Users/lillycham/Library/Python/3.9/bin /usr/local/plan9/bin /Library/TeX/texbin /Users/lillycham/.ghcup/bin /usr/local/bin /opt/local/bin /usr/bin /bin /usr/sbin /sbin .)\n	prompttext=`{ whoami }\n	prompt=(`{ whoami }' @ mirai % ' '    ')\n	NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt\n";
-}
--- config/pwsh.nix
+++
@@ -1,6 +0,0 @@
-{
-  home.file.".config/powershell/Microsoft.Powershell_profile.ps1".text = ''
-    Invoke-Expression(&starship init powershell)
-    Write-Host 'Welcome to PowerShell' (Get-Host).Version '.'
-  '';
-}
