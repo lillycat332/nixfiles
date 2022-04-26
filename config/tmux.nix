b822bb434b01d97d755c6dca45340c5f125e52d1
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-26 11:05:28 +0100

    Update tmux config

--- config/tmux.nix
+++ config/tmux.nix
@@ -10,12 +10,15 @@
       set -g pane-active-border-style fg='#ffb6c1'
 
       # Statusbar style
+      set-window-option -g status-right "%a %d %B %H:%M"
+      set-window-option -g status-left " #S "
+      set-window-option -g window-status-format " #I: #W "
+      set-window-option -g window-status-current-format " #I: #W "
       set -g status-style bg='#ffb6c1',fg=default
       set -g status-interval 1
-
+      
       # Message Style
       set -g message-style bg='#ffb6c1',fg=default
-
     '';
   };
 }
