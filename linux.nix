3d6ee444b9261c6c7ab40bdec65adcd95a079842
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-30 18:10:33 +0100

    update nixos.nix

--- linux.nix
+++ linux.nix
@@ -1,6 +1,7 @@
 {
 	# Import common.nix, with all of our packages and configs, then define any linux "platform specific" configs in here
 	imports = [ ./common.nix ];
+	pkgs = import sources.nixpkgs-unstable { };
 	home.username = ("lillycham");
   home.homeDirectory = "/home/lillycham";
 	home.packages = with pkgs; [
