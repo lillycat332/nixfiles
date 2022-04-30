6d49824edae99d8db8e3a461a9cd63627b6ae6c9
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-30 18:11:21 +0100

    update nixos.nix

--- nixos.nix
+++ nixos.nix
@@ -1,9 +1,10 @@
 {
 	# Import common.nix, with all of our packages and configs, then define any linux "platform specific" configs in here
 	imports = [ ./common.nix ];
+	pkgs = import sources.nixpkgs-unstable { };
 	home.username = ("lillycham");
   home.homeDirectory = "/home/lillycham";
-	home.packages = with nixos; [
+	home.packages = with pkgs; [
 		chromium
 		doas
 		clang
