6c0860a74d38a71e79806dc548202ff1c5d90a24
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-30 18:05:43 +0100

    add nixos.nix

---
+++ nixos.nix
@@ -0,0 +1,11 @@
+{
+	# Import common.nix, with all of our packages and configs, then define any linux "platform specific" configs in here
+	imports = [ ./common.nix ];
+	home.username = ("lillycham");
+  home.homeDirectory = "/home/lillycham";
+	home.packages = with nixos; [
+		chromium
+		doas
+		clang
+	];
+}
