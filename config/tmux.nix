9a684a1332a5cdbdd455891b071b622b7a7d5bbd
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-05-02 01:55:53 +0100

    Update tmux conf

--- config/tmux.nix
+++ config/tmux.nix
@@ -19,6 +19,14 @@
       
       # Message Style
       set -g message-style bg='#ffb6c1',fg=default
+
+      # List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+
+	# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+	run '~/.tmux/plugins/tpm/tpm'
     '';
   };
 }
