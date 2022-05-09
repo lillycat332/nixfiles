d958ffe5a4b2b2ca6577c4c009921bb151641816
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-05-09 11:41:23 +0100

    h

--- config/plan9port/plumber-plan9.nix
+++ config/plan9port/plumber-plan9.nix
@@ -1,12 +1,18 @@
 {config, pkgs, lib, ...}:
 {
-  home.file."lib/plumber".text = ''
-    # open pdf with `open`
+  home.file."lib/plumbing".text = ''
+    plan9=/usr/local/plan9
+    editor=acme
+    addrelem='((#?[0-9]+)|(/[A-Za-z0-9_\^]+/?)|[.$])'
+    addr=:($addrelem([,;+\-]$addrelem)*)
+    twocolonaddr=([0-9]+)[:.]([0-9]+)
+
+    # image files go to open
     type is text
-    data matches '[a-zA-Z¡-￿0-9_\-./]+'
-    data matches '([a-zA-Z¡-￿0-9_\-./]+)\.(ps|PS|eps|EPS|pdf|PDF|dvi|DVI)'
-    arg isfile	$0
-    plumb to postscript
+    data matches '[a−zA−Z0−9_\−./]+'
+    data matches '([a−zA−Z0−9_\−./]+).(jpe?g|gif|bit)'
+    arg isfile $0
+    plumb to image
     plumb start open $file
 
     # show git log
@@ -16,11 +22,5 @@
     data set $dir
     plumb start bash -c 'cd '$dir'; git show '$1' | 9p write acme/new/body'
 
-    # show git log
-    type is text
-    data matches 'commit ([a-z0-9]*)'
-    arg isdir .
-    data set $dir
-    plumb start bash -c 'cd '$dir'; git show '$1' | 9p write acme/new/body'
   '';
 }
